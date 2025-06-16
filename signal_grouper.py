bee85d4bcd0051129262f01f3f133f1d682c3941
Simon Blanco <siblor@mailbox.org>
2025-06-16 10:29:24 +0200

    rob_entries

--- signal_grouper.py
+++ signal_grouper.py
@@ -1,74 +1,86 @@
-# Define the range of slots you want to generate
-num_slots = 20  # Adjust this to generate more slots (up to 19 slots as per your original request)
+# Define the range of banks
+num_banks = 2  # Adjust this to generate more slots (up to 19 slots as per your original request)
 
-# Define the initial session group ID
-initial_session_group = 30  # You can modify this to start at any group ID
+# Define the range of banks
+num_entries = 32  # Adjust this to generate more slots (up to 19 slots as per your original request)
 
-signal_prefix = 'TestDriver.testHarness.chiptop.system.tile_prci_domain.tile_reset_domain.boom_tile.core.int_issue_unit.issue_slots'
+# Group
+initial_session_group = 159  # You can modify this to start at any group ID
+base_group_base_name = "idx "
+
+# Parent group
+parent_group = 158
+parent_name = "Entries"
+
+# Wave name
+wave_name = "Wave.1"
+
+#signal_prefix = 'TestDriver.testHarness.chiptop.system.tile_prci_domain.tile_reset_domain.boom_tile.core.int_issue_unit.issue_slots'
+signal_prefix = 'TestDriver.testHarness.chiptop.system.tile_prci_domain.tile_reset_domain.boom_tile.core.rob'
 
 # Base template for generating the code
-base_signal_1 = '''{signal_prefix}_{slot}_grant \
-{signal_prefix}_{slot}_in_uop_valid \
-{signal_prefix}_{slot}_request \
-{signal_prefix}_{slot}_uop_br_mask \
-{signal_prefix}_{slot}_uop_br_tag \
-{signal_prefix}_{slot}_uop_bypassable \
-{signal_prefix}_{slot}_uop_dst_rtype \
-{signal_prefix}_{slot}_uop_edge_inst \
-{signal_prefix}_{slot}_uop_fp_val \
-{signal_prefix}_{slot}_uop_ftq_idx \
-{signal_prefix}_{slot}_uop_fu_code \
-{signal_prefix}_{slot}_uop_imm_packed \
-{signal_prefix}_{slot}_uop_is_amo \
-{signal_prefix}_{slot}_uop_is_br \
-{signal_prefix}_{slot}_uop_is_jal \
-{signal_prefix}_{slot}_uop_is_jalr \
-{signal_prefix}_{slot}_uop_is_rvc \
-{signal_prefix}_{slot}_uop_is_sfb \
-{signal_prefix}_{slot}_uop_iw_p1_poisoned \
-{signal_prefix}_{slot}_uop_iw_p2_poisoned \
-{signal_prefix}_{slot}_uop_ldq_idx \
-{signal_prefix}_{slot}_uop_ldst_val \
-{signal_prefix}_{slot}_uop_lrs1_rtype \
-{signal_prefix}_{slot}_uop_lrs2_rtype \
-{signal_prefix}_{slot}_uop_mem_cmd \
-{signal_prefix}_{slot}_uop_pc_lob \
-{signal_prefix}_{slot}_uop_pdst \
-{signal_prefix}_{slot}_uop_prs1 \
-{signal_prefix}_{slot}_uop_prs2 \
-{signal_prefix}_{slot}_uop_rob_idx'''
+base_signal = '''{signal_prefix}.rob_val_{bank}_{entry} \
+{signal_prefix}.rob_bsy_{bank}_{entry} \
+{signal_prefix}.rob_unsafe_{bank}_{entry} \
+{signal_prefix}.rob_exception_{bank}_{entry} \
+{signal_prefix}.rob_predicated_{bank}_{entry} \
+{signal_prefix}.rob_uop_{bank}_{entry}_uopc \
+{signal_prefix}.rob_uop_{bank}_{entry}_is_rvc \
+{signal_prefix}.rob_uop_{bank}_{entry}_is_br \
+{signal_prefix}.rob_uop_{bank}_{entry}_is_jalr \
+{signal_prefix}.rob_uop_{bank}_{entry}_is_jal \
+{signal_prefix}.rob_uop_{bank}_{entry}_br_mask \
+{signal_prefix}.rob_uop_{bank}_{entry}_ftq_idx \
+{signal_prefix}.rob_uop_{bank}_{entry}_edge_inst \
+{signal_prefix}.rob_uop_{bank}_{entry}_pc_lob \
+{signal_prefix}.rob_uop_{bank}_{entry}_rob_idx \
+{signal_prefix}.rob_uop_{bank}_{entry}_pdst \
+{signal_prefix}.rob_uop_{bank}_{entry}_stale_pdst \
+{signal_prefix}.rob_uop_{bank}_{entry}_is_fencei \
+{signal_prefix}.rob_uop_{bank}_{entry}_uses_ldq \
+{signal_prefix}.rob_uop_{bank}_{entry}_uses_stq \
+{signal_prefix}.rob_uop_{bank}_{entry}_is_sys_pc2epc \
+{signal_prefix}.rob_uop_{bank}_{entry}_flush_on_commit \
+{signal_prefix}.rob_uop_{bank}_{entry}_ldst \
+{signal_prefix}.rob_uop_{bank}_{entry}_ldst_val \
+{signal_prefix}.rob_uop_{bank}_{entry}_dst_rtype \
+{signal_prefix}.rob_uop_{bank}_{entry}_fp_val \
+{signal_prefix}.rob_uop_{bank}_{entry}_debug_fsrc'''
 
-base_signal_2 = '''{signal_prefix}_{slot}_uop_taint \
-{signal_prefix}_{slot}_uop_taken \
-{signal_prefix}_{slot}_uop_uopc \
-{signal_prefix}_{slot}_uop_uses_stq \
-{signal_prefix}_{slot}_uop_yrot \
-{signal_prefix}_{slot}_valid \
-{signal_prefix}_{slot}_will_be_valid'''
 
-base_group = '''gui_list_add_group -id ${{Wave.1}} -after {{Issue Unit|Slot {slot}}} {{{{Issue Unit|Slot {next_slot}}}}}'''
+base_group = '''gui_list_add_group -id ${{{wave_name}}} -after {{{parent_name}|{base_group_base_name}{idx:02d}}} {{{{{parent_name}|{base_group_base_name}{next_idx:02d}}}}}'''
 
 # Function to generate the script for each slot
-def generate_slot_code(slot_number, session_group_start):
-    code = f'''# Issue slot {slot_number}
-set _session_group_{session_group_start + slot_number} $_session_group_{session_group_start - 1}|
-append _session_group_{session_group_start + slot_number} {{Slot {slot_number}}}
-gui_sg_create "$_session_group_{session_group_start + slot_number}"
-set {{Issue Unit|Slot {slot_number}}} "$_session_group_{session_group_start + slot_number}"
+def generate_groups_and_signals(bank, entry):
+    idx = (entry << 1) + bank
+    if (bank==0):
+        bank_string = ""
+    else:
+        bank_string = bank
 
-gui_sg_addsignal -group "$_session_group_{session_group_start + slot_number}" {{ {base_signal_1.format(signal_prefix=signal_prefix, slot=slot_number)} }}
-gui_sg_addsignal -group "$_session_group_{session_group_start + slot_number}" {{ {base_signal_2.format(signal_prefix=signal_prefix, slot=slot_number)} }}
+    code = f'''# Rob entry {idx:02d}
+set _session_group_{initial_session_group + idx} $_session_group_{parent_group}|
+append _session_group_{initial_session_group + idx} {{idx {idx:02d}}}
+gui_sg_create "$_session_group_{initial_session_group + idx}"
+set {parent_name}|{base_group_base_name}{idx:02d} "$_session_group_{initial_session_group + idx}"
+
+gui_sg_addsignal -group "$_session_group_{initial_session_group + idx}" {{ {base_signal.format(signal_prefix=signal_prefix, bank=bank_string, entry=entry, base_group=base_group, parent_name=parent_name, base_group_base_name=base_group_base_name)} }}
 '''
     return code
 
 # Generate and print the code for all slots
 print('### Signals to groups')
-for slot in range(num_slots):
-    print(generate_slot_code(slot, initial_session_group))
+for e in range(num_entries):
+    for b in range(num_banks):
+        idx = (e << 1) + b
+        #print("b: {b}    e:{e}   idx:{idx:02d}".format(b=b, e=e, idx=idx))
+        print(generate_groups_and_signals(bank=b, entry=e))
 
 print('### Groups')
 
-for slot in range(num_slots):
-    print(base_group.format(slot=slot, next_slot=slot+1))
+for e in range(num_entries):
+    for b in range(num_banks):
+        idx = (e << 1) + b
+        print(base_group.format(wave_name=wave_name, parent_name=parent_name, base_group_base_name=base_group_base_name, idx=idx, next_idx=idx+1))
 
 print('\nDone.')
