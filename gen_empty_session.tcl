3fe5b751168b58e0e8cb4acb493f46e42e0693dc
Simon Blanco <siblor@mailbox.org>
2025-10-27 15:48:49 +0100

    gen script fix

--- gen_empty_session.tcl
+++ gen_empty_session.tcl
@@ -1,8 +1,13 @@
-# Generate Wave view in a new toplevel and save the session
-set newWave [gui_create_window -type Wave -parent [gui_create_window -type TopLevel]]
-gui_show_window -window $newWave -show_state maximized
+# Generate view in a new toplevel
+set TopLevel1 [gui_create_window -type TopLevel]
+set Wave1 [gui_create_window -type Wave -parent $TopLevel1]
+
+# Open new window
+gui_show_window -window $Wave1 -show_state maximized
+
+# Save
+gui_save_session -file empty_wave_session.tcl -post
+
 # For old versions (<2019) this requires manual confirmation. Just click OK when prompt to exit
 # exit -force
 exit
-
-
